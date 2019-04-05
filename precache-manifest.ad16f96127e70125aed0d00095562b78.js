6c579d8049a7ad27c93be05b215f22eb5f512ee0
MCullers <cullers14@gmail.com>
2019-04-05 08:17:25 -0500

    Updates

---
+++ precache-manifest.ad16f96127e70125aed0d00095562b78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "342f71ab1a76a01dc97e",
+    "url": "/memory-game/static/js/main.a5c7d5e9.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/memory-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "342f71ab1a76a01dc97e",
+    "url": "/memory-game/static/css/main.4ada044d.chunk.css"
+  },
+  {
+    "revision": "0b32af72a49700de50e88ee92ad67095",
+    "url": "/memory-game/index.html"
+  }
+];
